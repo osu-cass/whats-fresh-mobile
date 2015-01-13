f740b5115c3775e8bfc87fd8bb6620470b391b2d
Free-BSG <michael.freeman@bsg-27.bus.oregonstate.edu>
2015-01-13 15:17:24 -0800

    Implement search util
    We implemented the search util so that the vendor store data would be
    sorted when we chose either a location or product. Note: this doesn't sort
    the vendor store data when the user location is enabled.

--- app/controller/List.js
+++ app/controller/List.js
@@ -8,8 +8,9 @@ Ext.define('WhatsFresh.controller.List', {
             'Ext.MessageBox',
             'Ext.device.Geolocation',
             'WhatsFresh.util.Link',
-            'WhatsFresh.util.Messages'
-        ],
+            'WhatsFresh.util.Messages',
+            'WhatsFresh.util.Geography',
+            'WhatsFresh.util.Search'],
 	alias: 'cont',
 	config: {
 		refs: {
@@ -119,7 +120,10 @@ Ext.define('WhatsFresh.controller.List', {
 		var vendorStore = Ext.data.StoreManager.lookup('Vendor');
 		var productStore = Ext.data.StoreManager.lookup('ProductList');
 
-            this.filterVendorStore(WhatsFresh.location, WhatsFresh.product);
+			WhatsFresh.util.Search.options.location = record._value.data;
+            // WhatsFresh.VendorStore.filter();
+            WhatsFresh.util.Search.applyFilterToStore(WhatsFresh.VendorStore);
+
 
 	    var homeView = this.getHomeView();
             homeView.getComponent('vendnum').setData(this.buildInventorySummary(WhatsFresh.location, WhatsFresh.product));
@@ -133,9 +137,10 @@ Ext.define('WhatsFresh.controller.List', {
 		WhatsFresh.product = record._value.data.name;
 		var vendorStore = Ext.data.StoreManager.lookup('Vendor');
 		var productStore = Ext.data.StoreManager.lookup('ProductList');
-		// console.log(store.data.all);
-		// console.log(store);
-            this.filterVendorStore(WhatsFresh.location, WhatsFresh.product);
+
+			WhatsFresh.util.Search.options.product = record._value.data;
+            // this.filterVendorStore(WhatsFresh.location, WhatsFresh.product);
+            WhatsFresh.util.Search.applyFilterToStore(WhatsFresh.VendorStore);
 
 	    var homeView = this.getHomeView();
             homeView.getComponent('vendnum').setData(this.buildInventorySummary(WhatsFresh.location, WhatsFresh.product));       
@@ -1076,6 +1081,9 @@ Ext.define('WhatsFresh.controller.List', {
 		WhatsFresh.StoryStore = Ext.getStore('Story');
 		WhatsFresh.VendorStore = Ext.getStore('Vendor');
 		WhatsFresh.VendorInventoryStore = Ext.getStore('VendorInventory');
+
+		// Applying filters to stores
+		WhatsFresh.util.Search.applyFilterToStore(WhatsFresh.VendorStore);
 
 		// Components
 			// ON: List page
