8abd9124192593a90fd5ef640786d7d107e016a0
Jacob Broderick <schmiddy373@gmail.com>
2015-01-21 13:11:59 -0800

    Add call functions to controller
    Add the functions to the controller that call the utilities that are created in a different commit. This will also add data to view elements that can be called by the utility functions.

--- app/controller/List.js
+++ app/controller/List.js
@@ -59,6 +59,7 @@ Ext.define('WhatsFresh.controller.List', {
 				viewIpageListItemCommand: 'onViewIpageListItemCommand'
 			},
 			specificView: {
+				videoTapFunction: 'onVideoTapFunction',
 				viewBackInfoCommand: 'onViewBackInfoCommand',
 				viewBackHomeCommand: 'onViewBackHomeCommand'
 			}
@@ -947,6 +948,10 @@ Ext.define('WhatsFresh.controller.List', {
 		console.log('In controller(info): View Specific Page Button');
 		Ext.Viewport.animateActiveItem(this.getSpecificView(), this.slideLeftTransition);
 	},	
+	onVideoTapFunction: function(link){
+		console.log('openening '+link);
+		WhatsFresh.util.Link.openVideo(link);
+	},
 	onViewIpageListItemCommand: function(record, list, index){
 		console.log('In controller(info): Selected');
 		// Ext.Msg.alert(index.data.listItem, 'This is the stuff I selected.');
@@ -1008,10 +1013,18 @@ Ext.define('WhatsFresh.controller.List', {
 					break;
 				case "Videos":
 					if(WhatsFresh.StoryStore.data.items[0].data.videos.length > 0){
+						//Pull out the video id ie www.youtube.com/v?=blablabla
+						//this pulls out the "blablabla" bit which is what we
+						//will use for most other stuff.
+						var link = WhatsFresh.util.Link.formatVideoLink(WhatsFresh.StoryStore.data.items[0].data.videos[0].link);
+
+
+						//Grab the link created so the view can use it
+						WhatsFresh.SVvideo.link = link;
+						WhatsFresh.SVvideo.setSrc('http://img.youtube.com/vi/'+ link +'/0.jpg');
 						WhatsFresh.SVvideo.show();
 						console.log('set the video');
 						console.log(WhatsFresh.SVvideo);
-						// WhatsFresh.SVvideo._url[0] = WhatsFresh.StoryStore.data.items[0].data.videos[0].link;
 					
 						var caption = {
 							cap: WhatsFresh.StoryStore.data.items[0].data.videos[0].caption
@@ -1095,7 +1108,7 @@ Ext.define('WhatsFresh.controller.List', {
 		// Components
 			// ON: List page
 			WhatsFresh.statmap = WhatsFresh.detailView.getComponent('staticmap');
-			console.log(WhatsFresh.statmap);		
+			console.log(WhatsFresh.statmap);
 			
 			// ON: Info page
 			WhatsFresh.INimage = WhatsFresh.infoView.getComponent('infoimage');
