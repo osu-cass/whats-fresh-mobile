b31676f49c6610545734ab879c95ba55a63eaecd
Free-BSG <michael.freeman@oregonstate.edu>
2015-02-27 15:14:32 -0800

    Add functionality to device back button
    We made it so that when the android back button was pressed, our app would
    act accordingly and navigate to the previous page. We did this by calling
    our appropriate back functions in the controller based on which view the
    user is currently in.

--- app.js
+++ app.js
@@ -41,6 +41,51 @@ Ext.application({
         Ext.Viewport.add(Ext.create('WhatsFresh.view.Info'));
         Ext.Viewport.add(Ext.create('WhatsFresh.view.Specific'));
 
+        // This is used to iplement android back button
+        if(Ext.os.is('Android')){
+            document.addEventListener("backButton", Ext.bind(onBackKeyDown, this), false);
+            function onBackKeyDown(eve){
+                eve.preventDefault();
+                console.log("device back button was pressed");
+                if(Ext.Viewport.getActiveItem().xtype ==  WhatsFresh.view.Home.xtype){
+                    navigator.app.exitApp();
+                }
+                // Action is like a fired event from a view page, routes are assigned
+                // for the url sent back in the controller routes section. The routes
+                // section in the controller defines which function to call when an 
+                // action is sent to the controller from the device back button
+                if(Ext.Viewport.getActiveItem().xtype ==  WhatsFresh.view.ListView.xtype){
+                    this.getApplication().getHistory().add(Ext.create('Ext.app.Action', {
+                        url: 'listback'
+                    }));
+                }
+                // Note after the device back button branch pull request is accepted and included
+                // in dev we will need to replace the detail view function with the commented
+                // out function below it in order to take into account navigation back from the
+                // productDetail page
+                if((Ext.Viewport.getActiveItem().xtype ==  WhatsFresh.view.Detail.xtype) | (Ext.Viewport.getActiveItem().xtype ==  WhatsFresh.view.ProductDetail.xtype)){
+                    this.getApplication().getHistory().add(Ext.create('Ext.app.Action', {
+                        url: 'detailback'
+                    }));
+                }
+                // if((Ext.Viewport.getActiveItem().xtype ==  WhatsFresh.view.Detail.xtype) | (Ext.Viewport.getActiveItem().xtype ==  WhatsFresh.view.Productdetail.xtype)){
+                //     this.getApplication().getHistory().add(Ext.create('Ext.app.Action', {
+                //         url: 'detailback'
+                //     }));
+                // }
+                if(Ext.Viewport.getActiveItem().xtype ==  WhatsFresh.view.Info.xtype){
+                    this.getApplication().getHistory().add(Ext.create('Ext.app.Action', {
+                        url: 'infoback'
+                    }));
+                }
+                if(Ext.Viewport.getActiveItem().xtype ==  WhatsFresh.view.Specific.xtype){
+                    this.getApplication().getHistory().add(Ext.create('Ext.app.Action', {
+                        url: 'specificback'
+                    }));
+                }
+            }
+        }
+
     },
 
     onUpdated: function() {
