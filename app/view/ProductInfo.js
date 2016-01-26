517ff2e8ef1e1dfbf5e312a5098cc3612fc20a16
Rob Mac <maccelerated@gmail.com>
2016-01-26 14:49:04 -0800

    Add management field
    This commit adds support for a ‘management’ field in the the stories
    API, as well as cleaning up formatting for a few files in the project.

--- app/view/ProductInfo.js
+++ app/view/ProductInfo.js
@@ -1,135 +1,125 @@
 Ext.define('OregonsCatch.view.ProductInfo', {
 	extend: 'Ext.Panel',
-	requires: ['Ext.Img'],
-    xtype: 'ProductInfoView',
+	requires: [
+		'Ext.Img'
+	],
+	xtype: 'ProductInfoView',
 	config: {
 		layout: 'vbox',
 		scrollable: {
 			vertical: true
 		},
-		items: [
-			{
-				xtype: 'toolbar',
-				docked: 'top',
-				title: 'Seafood',
-				itemId: 'Title',
-				items: [
-					{
-						xtype: 'button',
-						itemId: 'BackButton',
-						ui: 'action',
-						iconCls: 'arrow_left',
-					},
-					{
-						xtype: 'spacer'
-					},
-					{
-						xtype: 'button',
-						itemId: 'HomeButton',
-						ui: 'action',
-						iconCls: 'search'
-					}
-				]
-			},
-			{
-				xtype: 'image',
-				itemId: 'Image',
-				maxWidth: '100%',
-				height: '240px',
-				margin: '8px',
-				styleHtmlContent: true
-			},
-			{
-				xtype: 'fieldset',
-				itemId: 'Details',
-				styleHtmlContent: true,
-				tpl:	'<tpl if="origin"><div><strong>Origin</strong><span style="float:right">{origin}</span></div></tpl>' +
-						'<tpl if="alt_name"><div><strong>Other Name(s)</strong><span style="float:right">{alt_name}</span></div></tpl>' +
-						'<tpl if="variety"><div><strong>Variety</strong><span style="float:right">{variety}</span></div></tpl>' +
-						'<tpl if="season"><div><strong>Season</strong><span style="float:right">{season}</span></div></tpl>' +
-						'<tpl if="available"><div><strong>Availability</strong><span style="float:right">Available</span></div></tpl>' +
-						'<tpl if="!available"><div><strong>Availability</strong><span style="float:right">Unavailable</span></div></tpl>' +
-						'<tpl if="market_price"><div><strong>Market Price</strong><span style="float:right">{market_price}</span></div></tpl>',
-				data: { variety: 'Loading...' }
-			},
-			{
-				xtype: 'fieldset',
-				itemId: 'Description',
-				styleHtmlContent: true,
-				tpl: '{description}',
-				data: { description: 'Loading...' }
-			},
-			{
-				xtype: 'panel',
-				itemId: 'Story',
-				hidden: true,
-				showAnimation: { type: 'fadeIn' },
-				hideAnimation: { type: 'fadeOut' },
-				items: [
-					{
-						xtype: 'fieldset',
-						itemId: 'Facts',
-						styleHtmlContent: true,
-						tpl: '{facts}'
-					},
-					{
-						xtype: 'fieldset',
-						itemId: 'Products',
-						styleHtmlContent: true,
-						tpl: '{products}'
-					},
-					{
-						xtype: 'fieldset',
-						itemId: 'Preparing',
-						styleHtmlContent: true,
-						tpl: '{preparing}'
-					},
-					{
-						xtype: 'fieldset',
-						itemId: 'Buying',
-						styleHtmlContent: true,
-						tpl: '{buying}'
-					},
-					{
-						xtype: 'fieldset',
-						itemId: 'History',
-						styleHtmlContent: true,
-						tpl: '{history}'
-					},
-					{
-						xtype: 'fieldset',
-						itemId: 'ImagesButtonField',
-						items: [
-							{
-								xtype: 'button',
-								itemId: 'ImagesButton',
-								html: 'Images'
-							}
-						]
-					},
-					{
-						xtype: 'fieldset',
-						itemId: 'VideosButtonField',
-						items: [
-							{
-								xtype: 'button',
-								itemId: 'VideosButton',
-								html: 'Videos'
-							}
-						]
-					}
-				]
-			},
-			{
-				xtype: 'fieldset',
-				items: [
-					{
-						xtype: 'button',
-						itemId: 'BuyButton',
-						html: 'Where to Buy'
-					}
-				]
+		items: [{
+			xtype: 'toolbar',
+			docked: 'top',
+			title: 'Seafood',
+			itemId: 'Title',
+			items: [{
+				xtype: 'button',
+				itemId: 'BackButton',
+				ui: 'action',
+				iconCls: 'arrow_left'
+			}, {
+				xtype: 'spacer'
+			}, {
+				xtype: 'button',
+				itemId: 'HomeButton',
+				ui: 'action',
+				iconCls: 'search'
+			}]
+		}, {
+			xtype: 'image',
+			itemId: 'Image',
+			maxWidth: '100%',
+			height: '240px',
+			margin: '8px',
+			styleHtmlContent: true
+		}, {
+			xtype: 'fieldset',
+			itemId: 'Details',
+			styleHtmlContent: true,
+			tpl: '<tpl if="origin"><div><strong>Origin</strong><span style="float:right">{origin}</span></div></tpl>' +
+				'<tpl if="alt_name"><div><strong>Other Name(s)</strong><span style="float:right">{alt_name}</span></div></tpl>' +
+				'<tpl if="variety"><div><strong>Variety</strong><span style="float:right">{variety}</span></div></tpl>' +
+				'<tpl if="season"><div><strong>Season</strong><span style="float:right">{season}</span></div></tpl>' +
+				'<tpl if="available"><div><strong>Availability</strong><span style="float:right">Available</span></div></tpl>' +
+				'<tpl if="!available"><div><strong>Availability</strong><span style="float:right">Unavailable</span></div></tpl>' +
+				'<tpl if="market_price"><div><strong>Market Price</strong><span style="float:right">{market_price}</span></div></tpl>',
+			data: {
+				variety: 'Loading...'
 			}
-		]
+		}, {
+			xtype: 'fieldset',
+			itemId: 'Description',
+			styleHtmlContent: true,
+			tpl: '{description}',
+			data: {
+				description: 'Loading...'
+			}
+		}, {
+			xtype: 'panel',
+			itemId: 'Story',
+			hidden: true,
+			showAnimation: {
+				type: 'fadeIn'
+			},
+			hideAnimation: {
+				type: 'fadeOut'
+			},
+			items: [{
+				xtype: 'fieldset',
+				itemId: 'Facts',
+				styleHtmlContent: true,
+				tpl: '{facts}'
+			}, {
+				xtype: 'fieldset',
+				itemId: 'Management',
+				styleHtmlContent: true,
+				tpl: '{management}'
+			}, {
+				xtype: 'fieldset',
+				itemId: 'History',
+				styleHtmlContent: true,
+				tpl: '{history}'
+			}, {
+				xtype: 'fieldset',
+				itemId: 'Buying',
+				styleHtmlContent: true,
+				tpl: '{buying}'
+			}, {
+				xtype: 'fieldset',
+				itemId: 'Preparing',
+				styleHtmlContent: true,
+				tpl: '{preparing}'
+			}, {
+				xtype: 'fieldset',
+				itemId: 'Products',
+				styleHtmlContent: true,
+				tpl: '{products}'
+			}, {
+				xtype: 'fieldset',
+				itemId: 'ImagesButtonField',
+				items: [{
+					xtype: 'button',
+					itemId: 'ImagesButton',
+					html: 'Images'
+				}]
+			}, {
+				xtype: 'fieldset',
+				itemId: 'VideosButtonField',
+				items: [{
+					xtype: 'button',
+					itemId: 'VideosButton',
+					html: 'Videos'
+				}]
+			}]
+		}, {
+			xtype: 'fieldset',
+			items: [{
+				xtype: 'button',
+				itemId: 'BuyButton',
+				html: 'Where to Buy'
+			}]
+		}]
 	}
 });
--- app/view/VendorInfo.js
+++ app/view/VendorInfo.js
@@ -1,100 +1,92 @@
 Ext.define('OregonsCatch.view.VendorInfo', {
 	extend: 'Ext.Panel',
-	requires: [],
-    xtype: 'VendorInfoView',
+	requires: [
+		'Ext.Img'
+	],
+	xtype: 'VendorInfoView',
 	config: {
 		layout: 'vbox',
-		items: [
-			{
-				xtype: 'toolbar',
-				docked: 'top',
-				title: 'Vendor',
-				itemId: 'Title',
-				items: [
-					{
-						xtype: 'button',
-						itemId: 'BackButton',
-						ui: 'action',
-						iconCls: 'arrow_left',
+		items: [{
+			xtype: 'toolbar',
+			docked: 'top',
+			title: 'Vendor',
+			itemId: 'Title',
+			items: [{
+				xtype: 'button',
+				itemId: 'BackButton',
+				ui: 'action',
+				iconCls: 'arrow_left'
+			}, {
+				xtype: 'spacer'
+			}, {
+				xtype: 'button',
+				itemId: 'HomeButton',
+				ui: 'action',
+				iconCls: 'search'
+			}]
+		}, {
+			flex: 0.6,
+			xtype: 'panel',
+			scrollable: true,
+			items: [{
+				xtype: 'fieldset',
+				itemId: 'Description',
+				styleHtmlContent: true,
+				tpl: '<strong>{name}</strong><br/>{description}',
+				data: {
+					description: 'Loading...'
+				}
+			}, {
+				xtype: 'fieldset',
+				itemId: 'Contact',
+				styleHtmlContent: true,
+				tpl: '<div><strong>Contact</strong><span style="float:right">{contact_name}</span></div>' +
+					'<tpl if="phone"><strong>Phone</strong><span style="float:right">{phone}</span></tpl>' +
+					'<tpl if="email"><strong>Email</strong><span style="float:right">{email}</span></tpl>' +
+					'<tpl if="website" style="text-align:center"><div><strong>Website</strong><span style="float:right"><a href="{website}">Open Website</a></span></div></tpl>' +
+					'<tpl if="hours"><strong>Hours</strong><span style="float:right">{hours}</span></tpl>'
+			}, {
+				xtype: 'fieldset',
+				itemId: 'Navigation',
+				items: [{
+					xtype: 'label',
+					itemId: 'Address',
+					styleHtmlContent: true,
+					style: {
+						'text-align': 'center'
 					},
-					{
-						xtype: 'spacer'
+					tpl: '{street}, {city}, {state}, {zip}'
+				}, {
+					xtype: 'image',
+					itemId: 'MapImage',
+					styleHtmlContent: true,
+					width: '200px',
+					height: '200px',
+					margin: '8px auto'
+				}, {
+					xtype: 'label',
+					styleHtmlContent: true,
+					style: {
+						background: 'rgba(0,50,255,0.1)',
+						'text-align': 'center'
 					},
-					{
-						xtype: 'button',
-						itemId: 'HomeButton',
-						ui: 'action',
-						iconCls: 'search'
-					}
-				]
-			},
-			{
-				flex: 0.6,
-				xtype: 'panel',
-				scrollable: true,
-				items: [
-					{
-						xtype: 'fieldset',
-						itemId: 'Description',
-						styleHtmlContent: true,
-						tpl: '<strong>{name}</strong><br/>{description}',
-						data: { description: 'Loading...' }
-					},
-					{
-						xtype: 'fieldset',
-						itemId: 'Contact',
-						styleHtmlContent: true,
-						tpl:	'<div><strong>Contact</strong><span style="float:right">{contact_name}</span></div>' +
-								'<tpl if="phone"><strong>Phone</strong><span style="float:right">{phone}</span></tpl>' +
-								'<tpl if="email"><strong>Email</strong><span style="float:right">{email}</span></tpl>' +
-								'<tpl if="website" style="text-align:center"><div><strong>Website</strong><span style="float:right"><a href="{website}">Open Website</a></span></div></tpl>' +
-								'<tpl if="hours"><strong>Hours</strong><span style="float:right">{hours}</span></tpl>',
-					},
-					{
-						xtype: 'fieldset',
-						itemId: 'Navigation',
-						items: [
-							{
-								xtype: 'label',
-								itemId: 'Address',
-								styleHtmlContent: true,
-								style: { 'text-align': 'center' },
-								tpl: '{street}, {city}, {state}, {zip}'
-							},
-							{
-								xtype: 'image',
-								itemId: 'MapImage',
-								styleHtmlContent: true,
-								width: '200px',
-								height: '200px',
-								margin: '8px auto'
-							},
-							{
-								xtype: 'label',
-								styleHtmlContent: true,
-								style: { background: 'rgba(0,50,255,0.1)', 'text-align': 'center' },
-								html: 'Tap map to open in Maps.'
-							}
-						]
-					}
-				]
-			},
-			{
-				xtype: 'toolbar',
-				title: 'This vendor sells:'
-			},
-			{
-				flex: 0.3,
-				xtype: 'list',
-				itemId: 'List',
-				loadingText: 'Loading...',
-				emptyText: 'No seafood available now.',
-				deferEmptyText: false,
-				itemTpl: '{preparation} {name}',
-				ui: 'normal',
-				pinHeaders: false,
-				onItemDisclosure: true
-			}
-		]
+					html: 'Tap map to open in Maps.'
+				}]
+			}]
+		}, {
+			xtype: 'toolbar',
+			title: 'This vendor sells:'
+		}, {
+			flex: 0.3,
+			xtype: 'list',
+			itemId: 'List',
+			loadingText: 'Loading...',
+			emptyText: 'No seafood available now.',
+			deferEmptyText: false,
+			itemTpl: '{preparation} {name}',
+			ui: 'normal',
+			pinHeaders: false,
+			onItemDisclosure: true
+		}]
 	}
 });
